202a8ad357c3850a9275ea0c3df0dfb34a463964
Rick Schoenmaker <rickschoenmaker@hotmail.com>
2020-03-05 11:31:24 +0100

    venv in git ignore

--- PeptideClassifier.py
+++ PeptideClassifier.py
@@ -1,7 +1,5 @@
 import csv
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+
 from sklearn import svm, metrics, model_selection,datasets,linear_model
 from sklearn.model_selection import train_test_split
 from matplotlib.colors import ListedColormap
@@ -15,10 +13,14 @@ with open('processed_data.csv', mode='r') as csv_file:
         #print(row)
         fastarecords.append(row)
 
+
 #remove header
 fastarecords.pop(0)
+print(fastarecords[0])
+print(len(fastarecords))
 
 X_train, X_test = model_selection.train_test_split(
     fastarecords, train_size=0.75,test_size=0.25)
-print ("X_train: ", X_train)
-print("X_test: ", X_test)
+# print ("X_train: ", X_train)
+# print("X_test: ", X_test)
+train_test_split(y, shuffle=False)

bbfd36068323f7aaed53a5cf92b42bdbecb27391
teunjakobs <31921422+teunjakobs@users.noreply.github.com>
2020-03-23 12:07:08 +0100

    Nieuw commentaar toegevoegd en uitgecomment code verwijderd

--- confusing matrix input.py
+++ confusing matrix input.py
@@ -1,74 +1,58 @@
 import csv
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import re
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn import svm, metrics, model_selection,datasets,linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 fastarecords = []
-
+#Open the file with the parsed data
 with open('processed_data.csv', mode='r') as csv_file:
     csv_rows = csv.reader(csv_file)
     for row in csv_rows:
-        #print(row)
         fastarecords.append(row)
 
 #remove header
 fastarecords.pop(0)
 
-
-# function to convert a DNA sequence string to a numpy array
-# converts to lower case, changes any non 'acgt' characters to 'n'
+# function to convert a Amino acid sequence string to a numpy array
+# converts to lower case, changes any non amino acid characters to 'n'
 def string_to_array(my_string):
     my_string = my_string.lower()
     my_string = re.sub('[^arndcfqeghilkmpstwyv]', 'z', my_string)
-    my_array = np.array(list(my_string))
-    return my_array
+    my_aminoarray = np.array(list(my_string))
+    return my_aminoarray
 
-# create a label encoder with 'acgtn' alphabet
+# create a label encoder with amino acid alphabet
 label_encoder = LabelEncoder()
 label_encoder.fit(np.array(['a','r','n','d','c','f','q','e','g','h','i','l','k','m','p','s','t','w','y','v','z']))
-
 
 # function to convert sequence strings into k-mer words, default size = 6 (hexamer words)
 def getKmers(sequence, size=6):
     return [sequence[x:x+size].lower() for x in range(len(sequence) - size + 1)]
 
-train_df = pd.DataFrame(fastarecords)
-del train_df[0]
+#delete first column from the dataframe as it should not be taken into consideration
+dataset = pd.DataFrame(fastarecords)
+del dataset[0]
 
-
-train_df['words'] = train_df.apply(lambda x: getKmers(x[1]), axis=1)
-train_set = train_df.drop([1], axis=1)
-#test_df = pd.DataFrame(X_test)
-#test_df['words'] = test_df.apply(lambda x: getKmers(x[1]), axis=1)
-#test_set = test_df.drop([1], axis=1)
+dataset['words'] = dataset.apply(lambda x: getKmers(x[1]), axis=1)
+train_set = dataset.drop([1], axis=1)
 
 train_texts = list(train_set['words'])
 for item in range(len(train_texts)):
     train_texts[item] = ' '.join(train_texts[item])
-y_h = train_set.iloc[:, 0].values         #y_h for test
-
-#train_texts = list(train_set['words'])
-#for item in range(len(train_texts)):
-   # train_texts[item] = ' '.join(train_texts[item])
-#y_c = train_set.iloc[:, 0].values                       # y_c for train
+y_h = train_set.iloc[:, 0].values
 
 # Creating the Bag of Words model using CountVectorizer()
 # This is equivalent to k-mer counting
 # The n-gram size of 4 was previously determined by testing
 cv = CountVectorizer(ngram_range=(4,4))
 train_fit= cv.fit_transform(train_texts)
-#train_fit = cv.transform(train_texts)
 
-# Splitting the human dataset into the training set and test set
+# Splitting the dataset into the training set and test set
 X_train, X_test, y_train, y_test = train_test_split(train_fit,
                                                     y_h,
                                                     test_size = 0.20,
@@ -79,7 +63,6 @@ X_train, X_test, y_train, y_test = train_test_split(train_fit,
 classifier = MultinomialNB(alpha=0.1)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
-
 
 print("Confusion matrix\n")
 print(pd.crosstab(pd.Series(y_test, name='Actual'), pd.Series(y_pred, name='Predicted')))
